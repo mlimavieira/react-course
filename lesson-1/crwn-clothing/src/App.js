7e032219ae15996548f0593a487bb2fa91fbc10f
Marcio Vieira <marcio.vieira@mastercard.com>
2019-12-30 14:24:38 +0000

    SignIn and SignUp initial implementation

--- lesson-1/crwn-clothing/src/App.js
+++ lesson-1/crwn-clothing/src/App.js
@@ -3,6 +3,8 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 import Header from './components/header/header.component';
+
+import SignInSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 
@@ -16,6 +18,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
+        <Route path='/signin' component={SignInSignUp} />
       </Switch>
     </div>
   );
