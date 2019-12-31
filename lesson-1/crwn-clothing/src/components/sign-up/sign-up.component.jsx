62a8a172404e0f1a27477120b3b15c0bf41becdc
Marcio Vieira <marcio.vieira@mastercard.com>
2019-12-31 18:30:16 +0000

    Impl email and password authentication

---
+++ lesson-1/crwn-clothing/src/components/sign-up/sign-up.component.jsx
@@ -0,0 +1,95 @@
+import React from 'react';
+import FormInput from '../form-input/form-input.component';
+import CustomButton from '../custom-button/custom-button.compoment';
+
+import { auth, createUserProfileDocument } from '../../firebase/firebase.utils';
+
+import './sign-up.styles.scss';
+
+class SignUp extends React.Component {
+
+    constructor() {
+        super();
+
+        this.state = {
+            displayName: '',
+            email: '',
+            password: '',
+            confirmPassword: ''
+        }
+    }
+
+    handleSubmit = async event => {
+        event.preventDefault();
+
+        const { displayName, email, password, confirmPassword} = this.state;
+
+        if(password != confirmPassword) {
+            alert("Passwords don't match!");
+            return;
+        }
+
+        try {
+
+            const {user} = await auth.createUserWithEmailAndPassword(email, password);
+
+            await createUserProfileDocument(user, {displayName});
+
+            this.setState(
+                {
+                    displayName: '',
+                    email: '',
+                    password: '',
+                    confirmPassword: ''
+                }
+            );
+
+        } catch(error) {
+            console.error(error);
+        }
+    };
+
+    handleChange = event => {
+        const {value, name } = event.target;
+        this.setState({[name]: value});
+    }
+
+    render() {
+        const { displayName, email, password, confirmPassword} = this.state;
+        return (
+            <div className='sign-up'>
+                <h2 className='title'>I do not have an account </h2>
+                <span>Sign up with your email and password</span>
+                
+                <form className='sign-up-form' onSubmit={this.handleSubmit}>
+                    <FormInput name='displayName' type='text' 
+                           handleChange={this.handleChange}
+                           value={displayName} required 
+                           label='Display Name'/>
+
+                    <FormInput name='email' type='email' 
+                           handleChange={this.handleChange}
+                           value={email} required 
+                           label='Email'/>
+
+                    <FormInput name='password' type='password' 
+                           handleChange={this.handleChange}
+                           value={password} required 
+                           label='Password'/>
+
+                    <FormInput name='confirmPassword' type='password' 
+                           handleChange={this.handleChange}
+                           value={confirmPassword} required 
+                           label='Confirm Password'/>
+
+                    <CustomButton type='submit'>SIGN UP</CustomButton>
+                </form>
+
+
+            </div>
+        );
+    }
+}
+
+export default SignUp;
+
--- lesson-1/crwn-clothing/src/firebase/firebase.utils.js
+++ lesson-1/crwn-clothing/src/firebase/firebase.utils.js
@@ -4,25 +4,48 @@ import 'firebase/auth';
 
 
 const config = {
-    apiKey: "AIzaSyB4pTpG7kORLhg6694WYlMU_g5y3fusw-w",
-    authDomain: "crwn-db-9e879.firebaseapp.com",
-    databaseURL: "https://crwn-db-9e879.firebaseio.com",
-    projectId: "crwn-db-9e879",
-    storageBucket: "crwn-db-9e879.appspot.com",
-    messagingSenderId: "642625760679",
-    appId: "1:642625760679:web:6c89b8d52d34b665a65a82",
-    measurementId: "G-96X17M41P2"
-  };
+  apiKey: "AIzaSyB4pTpG7kORLhg6694WYlMU_g5y3fusw-w",
+  authDomain: "crwn-db-9e879.firebaseapp.com",
+  databaseURL: "https://crwn-db-9e879.firebaseio.com",
+  projectId: "crwn-db-9e879",
+  storageBucket: "crwn-db-9e879.appspot.com",
+  messagingSenderId: "642625760679",
+  appId: "1:642625760679:web:6c89b8d52d34b665a65a82",
+  measurementId: "G-96X17M41P2"
+};
 
-  firebase.initializeApp(config);
+export const createUserProfileDocument = async (userAuth, additionalData) => {
 
-  export const auth = firebase.auth();
-  export const firestore = firebase.firestore();
+  if (!userAuth) return;
 
-  const provider = new firebase.auth.GoogleAuthProvider();
-  provider.setCustomParameters({prompt: 'select_account'});
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+  const snapShot = await userRef.get();
 
-  export const signInWithGoogle = () => auth.signInWithPopup(provider);
+  if(!snapShot.exists) {
+    const { displayName, email} = userAuth;
+    const createAt = new Date();
+
+    try {
+
+      await userRef.set({displayName, email, createAt, ...additionalData});
+
+    } catch(error) {
+      console.log('Error creating user: ', error.message);
+    }
+  }
+
+  return userRef;
+}
+
+firebase.initializeApp(config);
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: 'select_account' });
+
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
 
 
-  export default firebase;
+export default firebase;
