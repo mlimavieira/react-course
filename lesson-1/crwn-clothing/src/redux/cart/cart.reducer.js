e37be628e9601ac67240b3994c0dfea44acbfd26
Marcio Vieira <marcio.vieira@mastercard.com>
2020-01-03 17:15:55 +0000

    Change shop.data, add cart.utils to handle cart logic

--- lesson-1/crwn-clothing/src/redux/cart/cart.reducer.js
+++ lesson-1/crwn-clothing/src/redux/cart/cart.reducer.js
@@ -1,6 +1,10 @@
 import CardActionsTypes from './cart.types';
+import CartActionsTypes from './cart.types';
+import {addItemToCart} from './cart.utils';
+
 const INITIAL_STATE = {
-    hidden: true
+    hidden: true,
+    cartItems: []
 };
 
 const cartReducer = (state = INITIAL_STATE, action) => {
@@ -9,6 +13,11 @@ const cartReducer = (state = INITIAL_STATE, action) => {
             return {
                 ...state,
                 hidden: !state.hidden
+            }
+        case CartActionsTypes.ADD_ITEM:
+            return {
+                ...state,
+                cartItems: addItemToCart(state.cartItems, action.payload)
             }
         default:
             return state;
